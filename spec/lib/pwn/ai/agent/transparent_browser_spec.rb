de0a762845b95aad43f67ee455c192217612a3e0
Jacob Hoopes <jake.hoopes@gmail.com>
2026-02-27 12:59:52 -0700

    RSpec #bugfixes for PWN::AI::Agent namespace

--- spec/lib/pwn/ai/agent/transparent_browser_spec.rb
+++ spec/lib/pwn/ai/agent/transparent_browser_spec.rb
@@ -3,9 +3,9 @@
 require 'spec_helper'
 
 describe PWN::AI::Agent::TransparentBrowser do
-  it 'scan method should exist' do
-    scan_response = PWN::AI::Agent::TransparentBrowser
-    expect(scan_response).to respond_to :scan
+  it 'analyze method should exist' do
+    analyze_response = PWN::AI::Agent::TransparentBrowser
+    expect(analyze_response).to respond_to :analyze
   end
 
   it 'should display information for authors' do
--- spec/lib/pwn/ai/agent_spec.rb
+++ spec/lib/pwn/ai/agent_spec.rb
@@ -2,7 +2,7 @@
 
 require 'spec_helper'
 
-describe Pwn::AI::Agent do
+describe PWN::AI::Agent do
   it 'should return data for help method' do
     help_response = Pwn::AI::Agent.help
     expect(help_response).not_to be_nil
